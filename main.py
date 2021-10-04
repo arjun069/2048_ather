7f5faa0bab56e151ae0e525281c0a0c8dcde99c7
Arjun <arjunnair2k18@gmail.com>
2021-10-04 21:59:07 +0530

    adding logic for directional shifting - left first

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 #4x4 matrix w random static values at the beginning 
 game_board = [[1024,2,4,8],[8,4,2,0],[0,0,2,0],[8,0,0,4]]
-def printLikeMatrix():
 
+
+sizeOfBoard = 4
+
+def printLikeMatrix():
 
 #finding out largest elemnt on board and then deciding number of spaces to make the board consistent
   largestElemOnBoard = game_board[0][0]
@@ -26,6 +29,31 @@ def printLikeMatrix():
   print()
 
 printLikeMatrix()
+
+
+#function to merge the current row towards the left
+def mergeL(row):
+  for j in range(sizeOfBoard-1):
+    for i in range(sizeOfBoard-1,0,-1):
+  #check if the before space is empty & if it is push/move everything to left
+      if row[i-1] == 0:
+        row[i-1]= row[i]
+        row[i] = 0
+
+  #merging happens now
+  for i in range(sizeOfBoard-1):
+    #check if next value and the current value are same
+    if row[i] == row[i+1]:
+      #if it is same then multiply the current value by 2 and assign the next value as 0
+      row[i]*=2
+      row[i+1] = 0 
+
+  
+  for i in range(sizeOfBoard-1,0,-1):
+#shift everything towards left again
+    if row[i-1] == 0:
+      row[i-1]= row[i]
+      row[i] = 0
 
 
 
